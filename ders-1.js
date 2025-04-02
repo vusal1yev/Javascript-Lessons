c7d455da87e9a62d4216422dd38ffebca3eac1fb
Vüsal Əliyev <mjolnir@Vusals-MacBook-Air.local>
2025-04-02 18:43:48 +0400

    intro

---
+++ ders-1.js
@@ -0,0 +1,35 @@
+x = 15
+y = 5
+d = x - y
+console.log(d)
+
+b = "Cabir"
+console.log(b)
+
+var vusal = ""
+let sagol = ''
+const salam = `salam`
+const salam = String("salam")
+
+number = 3.1
+
+// Comment
+// salam asdhjlashd asjkhdljasdjhlkasd
+
+/*
+
+*/
+
+// Javascript
+// https://www.w3schools.com/js/default.asp
+
+//
+
+// Programing Paradigm
+// https://dev.to/dchhitarka/introduction-to-programming-what-is-programming-paradigm-3le1
+
+// Compiler & Interpreter
+// https://academy.patika.dev/blogs/detail/compiler-ve-interpreter
+
+// Github
+// https://coderspace.io/sozluk/github/
